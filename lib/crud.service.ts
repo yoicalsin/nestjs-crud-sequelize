f33c9afaeab256259f9d30640cd7e703f137b5da
Yoni Calsin <yonicb20@gmail.com>
2020-04-11 22:32:54 -0500

    style: 💄 Aggregated model type for it will return  !

--- lib/crud.service.ts
+++ lib/crud.service.ts
@@ -6,25 +6,26 @@ import {
    BuildOptions,
    UpdateOptions,
 } from 'sequelize/types';
-export class CrudService<T = More> {
-   model: ModelCtor<Model<T, T>>;
 
-   constructor(model: ModelCtor<Model<T, T>>) {
+export class CrudService<T extends any = ModelCtor<Model>> {
+   model: ModelCtor<Model<any, any>>;
+
+   constructor(model: any) {
       this.model = model;
    }
 
-   async findAll(options: FindOptions = {}): Promise<Model<T, T>[]> {
-      const data = await this.model.findAll(options);
+   async findAll(options: FindOptions = {}): Promise<T[]> {
+      const data = (await this.model.findAll(options)) as any;
       return data;
    }
 
-   async findOne(options: FindOptions): Promise<Model<T, T>> {
-      const data = await this.model.findOne(options);
+   async findOne(options: FindOptions): Promise<T> {
+      const data = (await this.model.findOne(options)) as any;
       return data;
    }
 
-   async create(input: More, options: BuildOptions = {}): Promise<Model<T, T>> {
-      const data = new this.model(input, options);
+   async create(input: More, options: BuildOptions = {}): Promise<T> {
+      const data = new this.model(input, options) as any;
       return await data.save();
    }
 
@@ -32,13 +33,13 @@ export class CrudService<T = More> {
       input: More,
       options: UpdateOptions,
       findOptions: FindOptions,
-   ): Promise<Model<T, T>> {
+   ): Promise<T> {
       await this.model.update(input, options);
       const data = await this.findOne(findOptions);
       return data;
    }
 
-   async delete(options: FindOptions): Promise<Model<T, T>> {
+   async delete(options: FindOptions): Promise<T> {
       const data = await this.findOne(options);
       data && (await data.destroy());
       return data;
