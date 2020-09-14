d528ff93a4c685333680844ba7fc9aac8f56b699
Yony Calsin <helloyonicb@gmail.com>
2020-09-14 11:37:24 -0500

    style: 💄 formated

--- index.d.ts
+++ index.d.ts
@@ -1,28 +1,44 @@
-import { BuildOptions, FindAndCountOptions, FindOptions, Model, ModelCtor, UpdateOptions } from 'sequelize/types';
+import {
+   BuildOptions,
+   FindAndCountOptions,
+   FindOptions,
+   Model,
+   ModelCtor,
+   UpdateOptions,
+} from 'sequelize/types';
 interface PaginateOptions extends FindAndCountOptions {
-    page?: number;
+   page?: number;
 }
 interface More<T = any> {
-    [key: string]: T;
+   [key: string]: T;
 }
 export declare class CrudService<T extends any = ModelCtor<Model>> {
-    model: T & ModelCtor<Model<any, any>>;
-    constructor(model: any);
-    paginate(options?: PaginateOptions): Promise<{
-        total: any;
-        page: number;
-        totalPages: number;
-        items: any;
-        next: number;
-        prev: number;
-    }>;
-    findAll(options?: FindOptions): Promise<T[]>;
-    findOne(options: FindOptions): Promise<T>;
-    create(input: More, options?: BuildOptions): Promise<T>;
-    updateOrCreate(where: {
-        where: More;
-    }, input: More): Promise<T>;
-    update(input: More, options: UpdateOptions, findOptions: FindOptions): Promise<T>;
-    delete(options: FindOptions): Promise<T>;
+   model: T & ModelCtor<Model<any, any>>;
+   constructor(model: any);
+   paginate(
+      options?: PaginateOptions,
+   ): Promise<{
+      total: any;
+      page: number;
+      totalPages: number;
+      items: any;
+      next: number;
+      prev: number;
+   }>;
+   findAll(options?: FindOptions): Promise<T[]>;
+   findOne(options: FindOptions): Promise<T>;
+   create(input: More, options?: BuildOptions): Promise<T>;
+   updateOrCreate(
+      where: {
+         where: More;
+      },
+      input: More,
+   ): Promise<T>;
+   update(
+      input: More,
+      options: UpdateOptions,
+      findOptions: FindOptions,
+   ): Promise<T>;
+   delete(options: FindOptions): Promise<T>;
 }
 export {};
